48d3e02ff699523737b6ebd7af4061cd8dfc4fae
Andrei Ponomarenko <3355844@gmail.com>
2017-12-28 00:23:36 +0200

    Chat

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 $(function () {
+    var socket = io();
+    $('#form-chat').submit(function(){
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    socket.on('chat message', function(msg){
+        console.log(msg);
+        $('#messages').append($('<li>').text(msg));
+    });
 
 //    GET/READ
     $('#get-button').on('click', function () {
         $.ajax({
+            // cache: false,
             url: 'products',
+            method: 'GET',
             contentType: 'application/json',
             success: function (res) {
                 var tbodyEL = $('tbody');
@@ -13,8 +25,8 @@ $(function () {
                     console.log('function res');
                     tbodyEL.append('\
                     <tr>\
-                    <td class="id">'+ product.id +'</td>\
-                    <td><input type="text" class="name form-control" value="'+ product.name+'"/></td>\
+                    <td class="id">' + product.id + '</td>\
+                    <td><input type="text" class="name form-control" value="' + product.name + '"/></td>\
                     <td>\
                     <button class="update-button btn btn-outline-success">UPDATE/PUT</button>\
                     <button class="delete-button btn btn-outline-danger">DELETE</button>\
@@ -28,34 +40,34 @@ $(function () {
 //  CREATE POST
     $('#create-form').on('submit', function (event) {
         console.log('Post button is pressed');
-       event.preventDefault();
-       var createInput = $('#create-input');
+        event.preventDefault();
+        var createInput = $('#create-input');
 
-       $.ajax({
-           url: '/products',
-           method: 'POST',
-           contentType: 'application/json',
-           data: JSON.stringify({name: createInput.val()}),
-           success: function (res) {
-               console.log(res);
-               createInput.val('');
-               $('#get-button').click();
-           }
-       })
+        $.ajax({
+            url: '/products',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({name: createInput.val()}),
+            success: function (res) {
+                console.log(res);
+                createInput.val('');
+                $('#get-button').click();
+            }
+        })
     });
 
 //  UPDATE/PUT
     $('table').on('click', '.update-button', function () {
         var rowEL = $(this).closest('tr');
         var id = rowEL.find('.id').text();
-        var newName= rowEL.find('.name').val();
+        var newName = rowEL.find('.name').val();
         console.log(newName);
         $.ajax({
-            url:'/products/' + id,
-            method:'PUT',
+            url: '/products/' + id,
+            method: 'PUT',
             contentType: 'application/json',
             data: JSON.stringify({newName: newName}),
-            success:function (res) {
+            success: function (res) {
                 console.log(res);
                 $('#get-button').click();
             }
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var PORT = process.env.PORT || 3000;
 
 var products = [
@@ -63,6 +65,19 @@ app.delete('/products/:id', function (req, res) {
     res.send('Successfully deleted product');
 });
 
-app.listen(PORT, function () {
+io.on('connection', function (socket) {
+    console.log('user connected');
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', function () {
+        console.log('user disconnect');
+    });
+
+
+});
+
+http.listen(PORT, function () {
     console.log('Server listen PORT: ' + PORT)
 });
